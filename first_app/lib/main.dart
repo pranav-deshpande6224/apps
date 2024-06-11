4bd3cb58dbca52a0f12062b4dec1e0c13ecf2c96
pranavdeshpande <deshpande6224@gmail.com>
2024-06-11 12:59:05 +0530

    Update main.dart
    initial_setup

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,29 +6,27 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello Everyone"),
-          backgroundColor: Colors.blue,
-        ),
-        body: const Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [Text("Pranav"), Text("Prakash")]),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {},
+        backgroundColor: const Color.fromARGB(255, 59, 36, 97),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.yellow, Colors.pink],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello world',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
         ),
       ),
     );
