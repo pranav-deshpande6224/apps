a377d26b28e563b1b1c585a2acd13a238f69ce72
pranavdeshpande <deshpande6224@gmail.com>
2024-06-11 22:39:24 +0530

    Update start_screen.dart
    quiz app started

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -6,34 +6,42 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 82, 50, 138),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 250,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text(
-              'Learn Flutter the fun way!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.amber, Colors.pink],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 250,
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Start Quiz'),
-            )
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Start Quiz'),
+              )
+            ],
+          ),
         ),
       ),
     );
