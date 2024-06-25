43e14a9ae86cb10a500e38d0e22aa2754020ff95
pranavdeshpande <deshpande6224@gmail.com>
2024-06-25 15:46:17 +0530

    challenge done

--- section11/lib/Screens/grocery_screen.dart
+++ section11/lib/Screens/grocery_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:section11/Data/dummy_items.dart';
+import 'package:section11/Widgets/grocery_list_Item.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: 
-    AppBar(
-      title: const Text('Groceries'),
-    ),);
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 5,
+        title: const Text('Groceries'),
+      ),
+      body: ListView.builder(
+          itemCount: groceryItems.length,
+          itemBuilder: (ctx, index) {
+            return GroceryListItem(
+              item: groceryItems[index],
+            );
+          }),
+    );
   }
 }
---
+++ section11/lib/Widgets/grocery_list_Item.dart
@@ -0,0 +1,29 @@
+import 'package:section11/Models/grocery_item.dart';
+import 'package:flutter/material.dart';
+
+class GroceryListItem extends StatelessWidget {
+  const GroceryListItem({super.key, required this.item});
+  final GroceryItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        height: 20,
+        width: 20,
+        color: item.category.color,
+      ),
+      title: Text(
+        item.name,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      trailing: Text(
+        '${item.quantity}',
+        style: const TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
